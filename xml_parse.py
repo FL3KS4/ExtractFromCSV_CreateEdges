5fbfc7d4c0e1db093d4ecc6ff551f1f95883c4fc
Lukas <lukasjochymek@gmail.com>
2020-11-24 12:12:46 +0100

    Update xml_parse.py

--- xml_parse.py
+++ xml_parse.py
@@ -17,19 +17,13 @@ for message in root.getchildren():
 
 emails = list(dict.fromkeys(emails))
 summary = []
-print(emails[665])
-print(emails[2189])
-print(emails[663])
-print(emails[41])
-print(emails[37])
-print(emails[1527])
 
-""" for message in root.getchildren():
+for message in root.getchildren():
     id = ["0","0"]
     sender = (message.attrib).get('Sender')
     id[0] = emails.index(sender)
     for recipient in message.getchildren():
         id[1] = emails.index(recipient.text)
         cswriter.writerow(id)
-         """
+         
 f.close()
